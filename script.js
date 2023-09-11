c72ba0bafc767b0877c390fbb160e02b2fb9210d
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-11 23:04:11 +0100

    Finalised logic

--- script.js
+++ script.js
@@ -1,66 +1,76 @@
 $(document).ready(function () {
-let currentDay = dayjs().format("dddd D MMM YYYY");
-$("#currentDay").text(`Today is: ${currentDay}`);
+  let currentDay = dayjs().format("dddd D MMM YYYY");
+  $("#currentDay").text(`Today is: ${currentDay}`);
 
-let workHours = [
-  "9 AM",
-  "10 AM",
-  "11 AM",
-  "12 PM",
-  "1 PM",
-  "2 PM",
-  "3 PM",
-  "4 PM",
-  "5 PM",
-];
+  let workHours = [
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+  ];
 
-let container = $(".container");
+  let container = $(".container");
 
-workHours.forEach(function (hour) {
-  let timeBlock = $("<div>").addClass("row time-block");
-  let hourCol = $("<div>").addClass("col-md-1 hour").text(hour);
-  let descriptionCol = $("<textarea>").addClass("col-md-10 description");
-  let saveBtnCol = $("<button>")
-    .addClass("col-md-1 saveBtn")
-    .html('<i class="fas fa-save"></i>');
+  workHours.forEach(function (hour) {
+    let timeBlock = $("<div>").addClass("row time-block");
+    let hourCol = $("<div>").addClass("col-md-1 hour").text(hour);
+    let descriptionCol = $("<textarea>").addClass("col-md-10 description");
+    let saveDeleteCol = $("<div>").addClass("col-md-1");
+    let saveBtnCol = $("<button>")
+      .addClass("col-md-1 saveBtn")
+      .html('<i class="fas fa-save"></i>');
+    let deleteBtnCol = $("<button>")
+      .addClass("col-md-1 deleteBtn")
+      .html('<i class="fas fa-trash"></i>');
+    saveDeleteCol.append(saveBtnCol, deleteBtnCol);
 
-  timeBlock.append(hourCol, descriptionCol, saveBtnCol);
-  container.append(timeBlock);
-});
+    timeBlock.append(hourCol, descriptionCol, saveDeleteCol);
+    container.append(timeBlock);
+  });
 
-function loadEvents() {
-  workHours.forEach(function(hour) {
-    let event = localStorage.getItem(hour);
-    $(".description").each(function() {
-      if ($(this).siblings(".hour").text() === hour) {
-        $(this).val(event);
+  $(".saveBtn").on("click", function () {
+    let hour = $(this).closest(".time-block").find(".hour").text();
+    let event = $(this).closest(".time-block").find(".description").val();
+    localStorage.setItem(hour, event);
+  });
+
+  $(".deleteBtn").on("click", function () {
+    let hour = $(this).closest(".time-block").find(".hour").text();
+    localStorage.removeItem(hour);
+    $(this).closest(".time-block").find(".description").val("");
+  });
+
+  function loadEvents() {
+    workHours.forEach(function (hour) {
+      let event = localStorage.getItem(hour);
+      $(".description").each(function () {
+        if ($(this).siblings(".hour").text() === hour) {
+          $(this).val(event);
+        }
+      });
+    });
+  }
+
+  loadEvents();
+
+  function updateColors() {
+    let currentHour = dayjs().format("h A");
+    $(".time-block").each(function () {
+      let blockHour = $(this).find(".hour").text();
+      if (blockHour === currentHour) {
+        $(this).addClass("present").removeClass("past future");
+      } else if (dayjs(blockHour, "h A").isBefore(dayjs(currentHour, "h A"))) {
+        $(this).addClass("past").removeClass("present future");
+      } else {
+        $(this).addClass("future").removeClass("past present");
       }
     });
-  });
-}
-
-loadEvents();
-
-$(".saveBtn").on("click", function () {
-  let hour = $(this).siblings(".hour").text();
-  let event = $(this).siblings(".description").val();
-  localStorage.setItem(hour, event);
-});
-
-function updateColors() {
-    let currentHour = dayjs().format('h A');
-    $('.time-block').each(function() {
-        let blockHour = $(this).find('.hour').text();
-        if (blockHour === currentHour) {
-            $(this).addClass('present').removeClass('past future');
-        } else if (dayjs(blockHour, 'h A').isBefore(dayjs(currentHour, 'h A'))) {
-            $(this).addClass('past').removeClass('present future');
-          } else {
-            $(this).addClass('future').removeClass('past present');
-          }
-    })
-}
-updateColors();
-setInterval(updateColors, 60000)
-
+  }
+  updateColors();
+  setInterval(updateColors, 60000);
 });
