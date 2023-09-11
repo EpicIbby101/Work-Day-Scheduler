617e5ac75444e48a0fe8d4e7727a9c8690147004
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-11 21:24:14 +0100

    Finished the app logic

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
+$(document).ready(function () {
 let currentDay = dayjs().format("dddd D MMM YYYY");
-$("#currentDay").text(`Today is: ${currentDay}`)
+$("#currentDay").text(`Today is: ${currentDay}`);
 
 let workHours = [
-    '9 AM',
-    '10 AM',
-    '11 AM',
-    '12 PM',
-    '1 PM',
-    '2 PM',
-    '3 PM',
-    '4 PM',
-    '5 PM',
+  "9 AM",
+  "10 AM",
+  "11 AM",
+  "12 PM",
+  "1 PM",
+  "2 PM",
+  "3 PM",
+  "4 PM",
+  "5 PM",
 ];
 
-let container = $('.container');
+let container = $(".container");
 
 workHours.forEach(function (hour) {
-    let timeBlock = $('<div>').addClass('row time-block');
-    let hourCol = $('<div>').addClass('col-md-1 hour').text(hour);
-    let descriptionCol = $('<textarea>').addClass('col-md-10 description');
-    let saveBtnCol = $('<button>').addClass('col-md-1 saveBtn').html('<i class="fas fa-save"></i>');
+  let timeBlock = $("<div>").addClass("row time-block");
+  let hourCol = $("<div>").addClass("col-md-1 hour").text(hour);
+  let descriptionCol = $("<textarea>").addClass("col-md-10 description");
+  let saveBtnCol = $("<button>")
+    .addClass("col-md-1 saveBtn")
+    .html('<i class="fas fa-save"></i>');
 
-    timeBlock.append(hourCol, descriptionCol, saveBtnCol);
-    container.append(timeBlock)
-})
+  timeBlock.append(hourCol, descriptionCol, saveBtnCol);
+  container.append(timeBlock);
+});
 
 function loadEvents() {
-    workHours.forEach(function (hour) {
-        let event = localStorage.getItem(hour);
-        $('.description').each(function() {
-            if ($(this). siblings('.hour').text() === hour) {
-                $(this).val(event);
-            }
-        })
-    })
+  workHours.forEach(function(hour) {
+    let event = localStorage.getItem(hour);
+    $(".description").each(function() {
+      if ($(this).siblings(".hour").text() === hour) {
+        $(this).val(event);
+      }
+    });
+  });
 }
 
 loadEvents();
 
-$('.saveBtn').on('click', function() {
-    let hour = $(this).siblings('hour').text();
-    let event = $(this).siblings('.description').val();
-    localStorage.setItem(hour, event);
-})
+$(".saveBtn").on("click", function () {
+  let hour = $(this).siblings(".hour").text();
+  let event = $(this).siblings(".description").val();
+  localStorage.setItem(hour, event);
+});
+
+function updateColors() {
+    let currentHour = dayjs().format('h A');
+    $('.time-block').each(function() {
+        let blockHour = $(this).find('.hour').text();
+        if (blockHour === currentHour) {
+            $(this).addClass('present').removeClass('past future');
+        } else if (dayjs(blockHour, 'h A').isBefore(dayjs(currentHour, 'h A'))) {
+            $(this).addClass('past').removeClass('present future');
+          } else {
+            $(this).addClass('future').removeClass('past present');
+          }
+    })
+}
+updateColors();
+setInterval(updateColors, 60000)
+
+});
